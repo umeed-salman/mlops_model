44c1417037c2e488584db17daed02382072b1e40
umeed-salman <89387431+umeed-salman@users.noreply.github.com>
2023-03-15 16:41:38 +0500

    Update server.py

--- server.py
+++ server.py
@@ -5,14 +5,13 @@ import pickle
 app = Flask(__name__)
 # Load the model
 model = pickle.load(open('finalized_model.pkl','rb'))
-@app.route('/api',methods=['POST'])
-def predict():
-    # Get the data from the POST request.
-    data = request.get_json(force=True)
+@app.route('/api/<exp>')
+def predict(exp):
     # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict([[np.array(data['exp'])]])
+    prediction = model.predict([[float(exp)]])
     # Take the first value of prediction
-    output = prediction[0]
+    output = prediction.item()
     return jsonify(output)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
